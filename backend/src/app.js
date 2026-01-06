25851a57976ec923e53d4b6eba12f09a4cc6e7ee
Nicholas <snownicholas2@gmail.com>
2026-01-06 11:06:26 +0100

    change sendFile path

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,7 @@ app.use('/auth', authRouter);
 
 app.get(/.*/, (req, res) => {
     res.sendFile(
-        path.join(__dirname, "../../frontend/public/index.html")
+        path.join(__dirname, "../frontend/public/index.html")
     );
 });
 
