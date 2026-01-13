b561793774b3086fb2f734651743385a48d580b0
Nicholas <snownicholas2@gmail.com>
2026-01-13 08:48:29 +0100

    remove mailer code

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,18 +1,18 @@
-const { Resend } = require('resend');
-require('dotenv').config()
+// const { Resend } = require('resend');
+// require('dotenv').config()
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async ({ to, subject, html }) => {
-    return await resend.emails.send({
-        from: process.env.RESEND_FROM,
-        to,
-        subject,
-        html
-    })
-}
+// const sendEmail = async ({ to, subject, html }) => {
+//     return await resend.emails.send({
+//         from: process.env.RESEND_FROM,
+//         to,
+//         subject,
+//         html
+//     })
+// }
 
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
 
 
